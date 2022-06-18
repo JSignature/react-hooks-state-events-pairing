615d281ac8dc4ee05ee52fedabca1744098a9e2e
Jimmy Andreas <jimmysignature@gmail.com>
2022-06-18 16:03:13 -0400

    Added comment list

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Comments = ({ comments }) => {
   console.log(comments)
-  return <div>Comments</div>
+  return (
+    <div>
+      <h1>{comments.length} Comments</h1>
+      {comments.map(comment => (
+        <div key={comment.id}>
+          <h3>{comment.user}</h3>
+          <p>{comment.comment}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Comments
